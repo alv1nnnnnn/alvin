e489a7305cd660a644be938ee9a167d4fd7a8fc8
Alvin <alvinsalle.nario@bicol-u.edu.ph>
2026-03-15 08:06:53 +0800

    fix: update avatar images to PNG format and adjust text sizes for better visibility

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// For Production
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -10,3 +11,18 @@ export default defineConfig({
         emptyOutDir: true,
     },
 });
+
+// For Local
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: 'resources/js/app.js',
+//             refresh: true,
+//         }),
+//         tailwindcss(),
+//     ],
+// });
